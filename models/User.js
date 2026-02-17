1038e4eabcbd036162daeb92336407489c0c3d3d
Rishabh Tewatia <rishabh1teotia@gmail.com>
2026-02-18 00:05:28 +0530

    refactor: remove explicit `next()` calls from user password hashing pre-save hook.

--- models/User.js
+++ models/User.js
@@ -45,12 +45,11 @@ const userSchema = new mongoose.Schema(
  * Pre-save hook: Hash password before saving.
  * Uses regular function (not arrow) because `this` must refer to the document.
  */
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
 
     const salt = await bcrypt.genSalt(12);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 /**
